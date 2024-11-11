3623be3886089ff25730c4890e0673eeffaff5db
Brendan De La Cruz <brendan.delacruz7@gmail.com>
2024-11-11 11:33:34 -0800

    inital commit

---
+++ photo-api.js
@@ -0,0 +1,53 @@
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector("#search-button");
+
+function fetchPhotos(query) {
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=tSXK6Rn64lPb0ViLcHAGxUJepAKgm_m2pmJ9iFguvAI`;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(xhttp.responseText);
+            displayPhotoApp(data);
+        } else if (this.readyState == 4) {
+            console.error("Error fetching photos:", this.status, this.statusText);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function displayPhotoApp(data) {
+    var photos = data.results;
+    var resultsContainer = document.querySelector("#results-container");
+
+    // Clear previous images
+    resultsContainer.innerHTML ='';
+
+    photos.forEach(photo => {
+        
+        var photoContainer = document.createElement("div");
+        photoContainer.innerHTML = `
+            <img class="displayed-images" src="${photo.urls.regular}">
+        `;
+        resultsContainer.appendChild(photoContainer);
+    });
+}
+
+searchForm.addEventListener("submit", function() {
+    const query = searchInput.value.trim();
+    if(query) {
+        fetchPhotos(query);
+    }
+});
+
+searchButton.addEventListener('click', () => {
+    const query = searchInput.value.trim();
+    if (query) {
+        fetchPhotos(query);
+    }
+});
+
+fetchPhotos("soccer");
+// https://api.unsplash.com/search/photos?page=1&query=nature&client_id=[myAccessKey] 
+// https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=[myAccessKey]
